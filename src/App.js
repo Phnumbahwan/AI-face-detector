0bbe57a715cef60194a021b7550e8f345c8e3d27
Free Wifi <quickerz59@gmail.com>
2023-01-01 15:32:22 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import './App.css';
+import {useEffect, useRef, useState} from "react";
+import * as faceapi from 'face-api.js';
+
+function App() {
+    const videoWidth = 480;
+    const videoHeight = 640;
+    const [initializing, setInitializing] = useState(false);
+    const videoRef = useRef();
+    const canvasRef = useRef();
+
+    useEffect(() => {
+        const loadModels = async () => {
+            const MODEL_URL = process.env.PUBLIC_URL + '/models';
+            setInitializing(true);
+            Promise.all([
+                faceapi.nets.tinyFaceDetector.loadFromUri(MODEL_URL),
+                faceapi.nets.faceLandmark68Net.loadFromUri(MODEL_URL),
+                faceapi.nets.faceRecognitionNet.loadFromUri(MODEL_URL),
+                faceapi.nets.faceExpressionNet.loadFromUri(MODEL_URL)
+            ]).then(startVideo);
+        }
+        loadModels();
+    },[])
+
+    const startVideo = () => {
+        const constraints = {
+            video: {}
+        };
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then((mediaStream) => {
+                const video = document.querySelector('video');
+                video.srcObject = mediaStream;
+                video.onloadedmetadata = () => {
+                    video.play();
+                };
+            })
+            .catch((err) => {
+                // always check for errors at the end.
+                console.error(`${err.name}: ${err.message}`);
+            });
+    }
+
+    const handleVideoOnPlay = () => {
+        setInterval( async () => {
+            if(initializing) {
+                setInitializing(false);
+            }
+            canvasRef.current.innerHTML = faceapi.createCanvasFromMedia(videoRef.current);
+            const displaySize = {
+                width: videoWidth,
+                height: videoHeight
+            }
+            faceapi.matchDimensions(canvasRef.current, displaySize);
+            const detections = await faceapi.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+            const resizedDetection = faceapi.resizeResults(detections, displaySize);
+            canvasRef.current.getContext("2d").clearRect(0,0, videoWidth, videoHeight);
+            faceapi.draw.drawDetections(canvasRef.current, resizedDetection);
+            faceapi.draw.drawFaceLandmarks(canvasRef.current, resizedDetection);
+            faceapi.draw.drawFaceExpressions(canvasRef.current, detections);
+        }, 100)
+    }
+
+  return (
+    <div className="App">
+        <div>
+            <p>{initializing ? "Initializing" : "Ready"}</p>
+            <video ref={videoRef} autoPlay muted onPlay={handleVideoOnPlay}
+                style={
+                    {
+                        position:"absolute",
+                        marginLeft:"auto",
+                        marginRight:"auto",
+                        left:0,
+                        right:0,
+                        textAlign:"center",
+                        zIndex:9,
+                        width:videoWidth,
+                        height:videoHeight
+                    }
+                }
+            />
+            <canvas ref={canvasRef}
+                    style={
+                        {
+                            position:"relative",
+                            marginLeft:"auto",
+                            marginRight:"auto",
+                            left:0,
+                            right:0,
+                            textAlign:"center",
+                            zIndex:9,
+                            width:videoWidth,
+                            height:videoHeight
+                        }
+                    }
+            />
+        </div>
+    </div>
+  );
+}
+
+export default App;
